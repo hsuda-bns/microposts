7f3f8a091162c417e0cf7b5fbc6baa8374cf4f3b
hiromi.suda <hsuda@bns-g.com>
2021-11-04 07:02:22 +0000

    user pages

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
+//ユーザ一覧とユーザ詳細
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
+
 // 認証
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
