9609669dd55a15af1e934cc8ec6acb9914f4cbbd
hiromi.suda <hsuda@bns-g.com>
2021-11-05 08:23:41 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+Route::get('/', 'MicropostsController@index');  //上書き
+    
 // ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
@@ -18,6 +20,7 @@ Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 //ユーザ一覧とユーザ詳細
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
 
 // 認証
@@ -25,7 +28,4 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
-Route::get('/', function () {
-    return view('welcome');
-    
-});
+
